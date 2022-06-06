8bf6f99c585226ea304cd183564103fba87f1c6a
Andy Longhurst <andy@longhurst.net>
2022-06-06 09:47:36 +0100

    added an initial test

---
+++ SqlOnly/Blog.Tests.Integration/BlogSqlAccelergreatComponent.cs
@@ -0,0 +1,23 @@
+﻿using Accelergreat.EntityFramework.Sqlite;
+using Blog.Data;
+using Blog.Data.Entities;
+
+namespace Blog.Tests.Integration;
+
+public class BlogSqlAccelergreatComponent : SqliteEntityFrameworkDatabaseComponent<BlogDbContext>
+{
+    public IReadOnlyCollection<User> Users { get; private set; } = Array.Empty<User>();
+
+    protected override async Task OnDatabaseInitializedAsync(BlogDbContext context)
+    {
+        // Create some users which will be available for every test execution
+        var user0 = new User("Leonardo");
+        var user1 = new User("Michelangelo");
+
+        context.Set<User>().AddRange(user0, user1);
+
+        await context.SaveChangesAsync();
+
+        Users = new[] { user0, user1 };
+    }
+}
