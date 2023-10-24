47c295c12469984e5d7274231e4a1e8180d427fb
Andy Longhurst <andy@longhurst.net>
2023-10-24 08:57:13 +0100

    add autofixture and autodataattribute

---
+++ SqlOnly/Blog.Tests.Integration/Customisation/AutoNSubstitudeDataAttribute.cs
@@ -0,0 +1,27 @@
+﻿using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using AutoFixture.Xunit2;
+
+namespace Blog.Tests.Integration.Customisation;
+
+public class AutoNSubstituteDataAttribute : AutoDataAttribute
+{
+    public AutoNSubstituteDataAttribute(params Type[] optionalCustomizationTypes)
+        : base(() => BuildFixture(optionalCustomizationTypes))
+    {
+    }
+
+    // ReSharper disable once ParameterTypeCanBeEnumerable.Local
+    private static IFixture BuildFixture(Type[] optionalCustomizationTypes)
+    {
+        var fixture = new Fixture()
+            .Customize(new AutoNSubstituteCustomization());
+
+        foreach (var customizationType in optionalCustomizationTypes)
+        {
+            fixture.Customize((ICustomization)Activator.CreateInstance(customizationType));
+        }
+
+        return fixture;
+    }
+}
